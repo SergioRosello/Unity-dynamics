df4039d4b8be2a73567481ac0094a19339fc3bce
BIGBADMONSTER\sergio <sergio-rosello@hotmail.com>
2018-05-11 13:02:04 +0200

    Acceleration-deceleration modifier done

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class PlayerMovementController : MovementController {
 	public GameObject BombPrefab;
+    protected float baseAcceleration;
+    protected float baseDeceleration;
+    [Range(0.0f, 1.0f)]
+    public float AccelerationJumpReducer;
+    [Range(0.0f, 1.0f)]
+    public float DecelerationJumpReducer;
 
-	protected override Vector2 GetDesiredMovement () {
+
+    protected override Vector2 GetDesiredMovement () {
 		return new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical")).normalized;
 	}
 
-	protected override void Update() {
+    protected override void Awake()
+    {
+        base.Awake();
+        baseAcceleration = Acceleration;
+        baseDeceleration = Deceleration;
+    }
+
+    protected override void Update() {
 		base.Update ();
-		if (Input.GetKeyDown (KeyCode.Space) && _groundCheck.Grounded) {
+
+        Acceleration = baseAcceleration;
+        Deceleration = baseDeceleration;
+        if (Input.GetKeyDown (KeyCode.Space) && _groundCheck.Grounded) {
 			// Salto
 			_rb.AddForce(Vector3.up * JumpForce, ForceMode.VelocityChange);
-		}
+        }
+        if (!_groundCheck.Grounded)
+        {
+            Debug.Log("Jumping...");
+            // Cambio de aceleracion-deceleracion en el aire
+            Acceleration *= AccelerationJumpReducer;
+            Deceleration *= DecelerationJumpReducer;
+        }
+
 
 		if (Input.GetKeyDown (KeyCode.Q)) {
 			Instantiate (BombPrefab, transform.position, Quaternion.identity);

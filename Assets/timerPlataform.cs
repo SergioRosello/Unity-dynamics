865a9fd775bb25d4ad8b4d9fe451caf5ea2c1144
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2018-05-25 12:07:16 +0200

    timerPlataforms fall and reset

--- Assets/timerPlataform.cs
+++ Assets/timerPlataform.cs
@@ -5,22 +5,51 @@ using UnityEngine;
 public class timerPlataform : MonoBehaviour {
 
     private bool playerCollided;
+    private bool hasToStartFalling;
+    private bool isInOriginalPosition;
+    private Vector3 originalPosition;
+    public float timeTillFall;
+    public float timeTillReset;
 	// Use this for initialization
 	void Start () {
+        isInOriginalPosition = true;
+        hasToStartFalling = false;
         playerCollided = false;
+        originalPosition = transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (playerCollided){
+        if (playerCollided && hasToStartFalling){
             transform.SetPositionAndRotation(new Vector3(transform.position.x, transform.position.y - .1f, transform.position.z), Quaternion.identity);
         }
     }
 
-    void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
             playerCollided = true;
             other.gameObject.GetComponentInChildren<GroundCheck>().Grounded = true;
+            StartCoroutine(Fall());
         }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") {
+            other.gameObject.GetComponentInChildren<GroundCheck>().Grounded = false;
+            StartCoroutine(Reset());
+        }
+    }
+
+    IEnumerator Fall() {
+        yield return new WaitForSeconds(timeTillFall);
+        hasToStartFalling = true;
+        isInOriginalPosition = false;
+    }
+
+    IEnumerator Reset() {
+        yield return new WaitForSeconds(timeTillReset);
+        transform.position = originalPosition;
+        playerCollided = false;
+        hasToStartFalling = false;
     }
 }
